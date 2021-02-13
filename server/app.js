4d48d2602a45a9fcd79751824cacf85435682147
KhoiNguyen <thorwaitson@gmail.com>
2021-02-12 19:00:08 -0600

    add error handling route and add calendar route

--- server/app.js
+++ server/app.js
@@ -10,12 +10,21 @@ app.use(bodyParser.json());
 app.use(cors());
 
 //Routes
+const calendarRoute = require("./routes/calendar-route");
+app.use("/api/calendar", calendarRoute);
+
 const reserveRoute = require("./routes/reserve-route");
 app.use("/api/reserve", reserveRoute);
 
 const feedbackRoute = require("./routes/feedback-route");
 app.use("/api/feedback", feedbackRoute);
 
+//Error handling
+app.use((err, re, res, next) => {
+  const status = err.status || 500;
+  res.status(status).send(err.message);
+});
+
 app.listen(PORT, () => {
-  console.log("On " + PORT);
+  console.log("On port " + PORT);
 });
