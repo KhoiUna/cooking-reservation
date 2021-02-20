38713b71e8808bbdad2108751a4392040656a8dd
KhoiNguyen <thorwaitson@gmail.com>
2021-02-20 13:47:10 -0600

    remove Calendar border top & update Home.js

--- client/src/containers/Home/Home.js
+++ client/src/containers/Home/Home.js
@@ -1,10 +1,27 @@
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { origin } from "../../config";
 import Header from "../../components/Header/Header";
 import Calendar from "../../components/Calendar/Calendar";
 import Footer from "../../components/Footer/Footer";
 import "./Home.css";
 
 export default function Home() {
+  const [numReservedObj, setNumReservedObj] = useState(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      let res = (async () =>
+        await fetch(`${origin}/api/calendar?dateIndex=0`))();
+
+      res
+        .then((r) => r.json())
+        .then((r) => {
+          setNumReservedObj(r);
+        });
+    }, 100);
+  });
+
   return (
     <div className="Home">
       <Header />
@@ -20,12 +37,20 @@ export default function Home() {
       <Calendar />
 
       <div className="flex-buttons">
-        <Link to="/reserve">
-          <button id="reserve-button">RESERVE</button>
-        </Link>
-        <Link to="/feedback">
-          <button id="feedback-button">FEEDBACK</button>
-        </Link>
+        {numReservedObj ? (
+          <>
+            <Link to="/reserve">
+              <button id="reserve-button">RESERVE</button>
+            </Link>
+            <Link to="/feedback">
+              <button id="feedback-button">FEEDBACK</button>
+            </Link>
+          </>
+        ) : (
+          <h3>
+            <i>Your RESERVE button will appear shortly. Please wait...</i>
+          </h3>
+        )}
       </div>
 
       <Footer />
