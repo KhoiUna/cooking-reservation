0b9f0c3f3df1e440c3cf049ee241c1bf0b2d78e1
KhoiNguyen <thorwaitson@gmail.com>
2021-02-25 11:51:09 -0600

    add useRef for first fetch of data

--- client/src/containers/Home/Home.js
+++ client/src/containers/Home/Home.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { origin } from "../../config";
 import Header from "../../components/Header/Header";
 import Calendar from "../../components/Calendar/Calendar";
@@ -14,8 +14,8 @@ export default function Home() {
     }
     if (move === "right") {
       setDateIndex(dateIndex + 1);
-      setNumReservedObj(null);
     }
+    setNumReservedObj(null);
   };
 
   const [numReservedObj, setNumReservedObj] = useState(null);
@@ -30,6 +30,9 @@ export default function Home() {
         });
     }, 100);
   }, [dateIndex]);
+
+  const firstFetch = useRef(true);
+  firstFetch.current = false;
 
   return (
     <div className="Home">
@@ -50,7 +53,7 @@ export default function Home() {
       />
 
       <div className="flex-buttons">
-        {numReservedObj ? (
+        {!firstFetch.current ? (
           <>
             <Link to="/reserve">
               <button id="reserve-button">RESERVE</button>
