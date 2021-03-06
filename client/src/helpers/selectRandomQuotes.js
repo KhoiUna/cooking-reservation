3fe751b3603e28d9bf6067727be2251a0f40c425
KhoiNguyen <thorwaitson@gmail.com>
2021-03-06 16:22:43 -0600

    update random quotes

--- client/src/helpers/selectRandomQuotes.js
+++ client/src/helpers/selectRandomQuotes.js
@@ -3,8 +3,12 @@ export default function selectRandomQuotes(firstName) {
     0: [firstName, ", thanks for reserving!"],
     1: [firstName, ", you look great today!"],
     2: [firstName, ", you are making Rice Hall better!"],
-    3: ["Someone will forever remember your name, ", firstName],
+    3: ["You will be forever remembered, ", firstName, "!"],
     4: [firstName, " and me will fight COVID together!"],
+    5: ["Ms. Cala is thankful for you, ", firstName, "!"],
+    6: ["Gracias, ", firstName, "!"],
+    7: ["谢谢 (thanks)", firstName, "!"],
+    8: ["ありがとう (thanks), ", firstName, "!"],
   };
 
   const randomIndex = Math.floor(Math.random() * Object.keys(quotes).length);
