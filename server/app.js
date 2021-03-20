02fe823660860f4958ea99e09f3bbbc50f984e4a
KhoiNguyen <thorwaitson@gmail.com>
2021-03-19 20:03:56 -0500

    allow cors *

--- server/app.js
+++ server/app.js
@@ -11,13 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.use(compression());
 app.use(helmet());
 app.use(express.json());
-app.use(
-  cors({
-    origin: origin,
-    optionsSuccessStatus: 200,
-    credentials: true,
-  })
-);
+app.use(cors());
 
 //Routes
 const calendarRoute = require("./routes/calendar-route");
