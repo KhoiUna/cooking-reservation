f965095970a7d0de8eba82e889bafc56be56b793
KhoiNguyen <thorwaitson@gmail.com>
2021-03-19 20:04:25 -0500

    change cors

--- server/app.js
+++ server/app.js
@@ -11,7 +11,13 @@ const PORT = process.env.PORT || 5000;
 app.use(compression());
 app.use(helmet());
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: origin,
+    optionsSuccessStatus: 200,
+    credentials: true,
+  })
+);
 
 //Routes
 const calendarRoute = require("./routes/calendar-route");
