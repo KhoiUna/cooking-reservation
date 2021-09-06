b78c618c1e69ab5ac393dd869291e23a46646154
KhoiNguyen <thorwaitson@gmail.com>
2021-09-06 11:34:28 -0500

    add voice synthesis feature

---
+++ client/helpers/selectAudioQuotes.js
@@ -0,0 +1,18 @@
+export default function selectAudioQuotes(index) {
+  const quotes = {
+    0: ["thanks for reserving!"],
+    1: ["you look great today!"],
+    2: ["you are making Rice Hall better!"],
+    3: ["You will be forever remembered"],
+    4: ["We will fight co-vidd together"],
+    5: ["Ms. Cala is thankful for you"],
+    6: ["Gracias"],
+    7: ["shia shia"],
+    8: ["arigato gozai mas"],
+    9: ["dhanyavaad"],
+    10: ["gam sah ah knee duh"],
+    11: ["Cảm ơn"],
+  };
+
+  return quotes[index];
+}
