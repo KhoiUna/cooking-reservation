3266dc1b789b2b5e3c3d3d361ca3a0b0ab0f509c
KhoiNguyen <thorwaitson@gmail.com>
2021-09-06 11:34:13 -0500

    add google ad

--- client/containers/layout.js
+++ client/containers/layout.js
@@ -17,6 +17,12 @@ export default function Layout({ children, componentName }) {
         <link rel="apple-touch-icon" href="/logo192.png" />
         <link rel="manifest" href="/manifest.json" />
 
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2815892623196438"
+          crossorigin="anonymous"
+        ></script>
+
         <title>GLC Cooking Reservation | {componentName}</title>
       </Head>
 
