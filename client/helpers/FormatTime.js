92133067adf01ab74e7a6d479c4bcefd72891a10
KhoiNguyen <thorwaitson@gmail.com>
2021-09-20 23:12:22 -0500

    update client helper FormatTime

--- client/helpers/FormatTime.js
+++ client/helpers/FormatTime.js
@@ -83,4 +83,22 @@ module.exports = {
     const dateName = dateNames[nameIndex];
     return `${dateName} - ${date}`;
   },
+  getDayOfWeek(date) {
+    switch (new Date(date).getDay()) {
+      case 0:
+        return "Sun";
+      case 1:
+        return "Mon";
+      case 2:
+        return "Tue";
+      case 3:
+        return "Wed";
+      case 4:
+        return "Thu";
+      case 5:
+        return "Fri";
+      case 6:
+        return "Sat";
+    }
+  },
 };
