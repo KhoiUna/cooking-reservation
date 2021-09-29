8bc2a6be92d77fa2da08d913115de004c3df48c5
KhoiUna <thorwaitson@gmail.com>
2021-09-29 10:47:24 -0500

    update theme color

--- client/containers/layout.js
+++ client/containers/layout.js
@@ -9,7 +9,7 @@ export default function Layout({ children, componentName }) {
         <meta charSet="utf-8" />
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="theme-color" content="#000000" />
+        <meta name="theme-color" content="#46166b" />
         <meta
           name="description"
           content="Reserve your cooking time slot here"
